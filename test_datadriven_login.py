fddab1f321cad7c002c29305cd29864ba9988528
Rishabh Sinha <saurav2846@gmail.com>
2025-08-27 12:09:00 +0530

    Added test data files and fixed data-driven login tests

--- test_datadriven_login.py
+++ test_datadriven_login.py
@@ -5,10 +5,13 @@ from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 # Load test data from JSON file
 def load_testdata():
-    with open(r'C:\Users\Rishabh Sinha\Documents\Selenium_python\testdata1.json', 'r') as f:
+    with open('testdata1.json', 'r') as f:
         return json.load(f)
 
 testdata = load_testdata()
@@ -22,10 +25,18 @@ def driver():
     yield driver
     driver.quit()
 
-@pytest.mark.parametrize("userdata", testdata)
+@pytest.mark.parametrize("userdata", testdata.values())
 def test_login_datadriven(driver, userdata):
+    wait = WebDriverWait(driver, 10)
     driver.get("https://practice.expandtesting.com/login")
-    driver.find_element(By.ID, "username").send_keys(userdata['name'])
-    driver.find_element(By.ID, "password").send_keys(userdata['password'])
-    driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-    assert userdata['expected'] in driver.page_source
+    
+    username_field = wait.until(EC.element_to_be_clickable((By.ID, "username")))
+    username_field.send_keys(userdata['username'])
+    
+    password_field = wait.until(EC.element_to_be_clickable((By.ID, "password")))
+    password_field.send_keys(userdata['password'])
+    
+    submit_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[type='submit']")))
+    driver.execute_script("arguments[0].click();", submit_button)
+    time.sleep(2)  # Wait for the response
+    assert userdata['expected_message'] in driver.page_source
